ac4213440c7d9b89e53265064e847f185e087b2c
Dmytro Kudryk <dmytro.kudryk@gmail.com>
2022-09-03 13:48:05 +0300

    ✨ add mobile lang support

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -47,7 +47,14 @@ function Navigation() {
                   alt=""
                 />
               </div>
-              <div className="flex items-center md:hidden">
+              <div className="flex items-center md:hidden space-x-6">
+                <div
+                  onClick={handleChangeLang}
+                  className="font-bold text-white hover:text-yellow-300 uppercase cursor-pointer"
+                  title={`${t('Switch to')} ${oppositeLanguage.toUpperCase()}`}
+                >
+                  {oppositeLanguage}
+                </div>
                 <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   <MenuIcon className="h-6 w-6" aria-hidden="true" />
@@ -119,7 +126,14 @@ function Navigation() {
               <div>
                 <img className="h-16 w-auto" src={WhiteLogo} alt="" />
               </div>
-              <div className="-mr-2">
+              <div className="-mr-2 flex items-center space-x-6">
+                <div
+                  onClick={handleChangeLang}
+                  className="font-bold uppercase cursor-pointer"
+                  title={`${t('Switch to')} ${oppositeLanguage.toUpperCase()}`}
+                >
+                  {oppositeLanguage}
+                </div>
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500">
                   <span className="sr-only">Close menu</span>
                   <XIcon className="h-6 w-6" aria-hidden="true" />
